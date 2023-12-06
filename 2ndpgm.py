580885d84277ba2be86b24af6686594157d16418
RevathySR <121613327+RevathySR@users.noreply.github.com>
2023-12-06 12:26:23 +0530

    2ndpgm.py

---
+++ 2ndpgm.py
@@ -0,0 +1,16 @@
+"""print("WELCOME TO SECOND PROGRAM")
+a=20
+print("a=",a)"""
+
+
+Name="Revathy S R"
+Address ="Srambikkal house \n Chelakkara (p.o)\n Thrissur \n Kerala"
+
+
+print("My name is:"+Name ,"\nAddress:"+Address)
+
+
+print("Number variables")
+regno=1000
+total=800
+print("Register number:"+str(regno)+"\nTOtal:"+str(total))
